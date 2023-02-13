58ff53ede721a94c9d8eaa09c642b9f1aff3ccb1
Mabulkhail <103129132+Mabulkhail@users.noreply.github.com>
2023-02-13 17:01:54 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,42 @@ from kivy.lang import Builder
 from kivymd.app import MDApp
 from kivy.uix.screenmanager import ScreenManager
 
+welc = """
+MDScreen:
+    name: 'welcome'
+    MDFloatLayout:
+        md_bg_color: 1, 1, 1, 1
+        
+        MDLabel:
+            text: "Indoor Navigation for Faculty Member floor"
+            font_size: "26sp"
+            pos_hint: {"center_x": 0.54, "center_y": .85}
+            color: rgba(0, 0, 59, 255)
 
+        MagicButton:
+            text: "Go as guest"
+            pos_hint: {"center_x": .5, "center_y": .14}
+            on_press:
+                self.grow()
+                root.manager.current = "main"
+                root.manager.transition.direction = "left"
+<MagicButton@MagicBehavior+Button>:
+    text:root.text
+    size_hint: .5, .065
+    pos_hint: root.pos_hint
+    background_color: 0, 0, 0, 0
+    on_press:
+        root.on_press
+    canvas.before:
+        Color:
+            rgb: rgba(52, 0, 231, 255)
+        RoundedRectangle:
+            size: self.size
+            pos: self.pos
+            radius: [5]
+"""
+
+minp = """
 class MainApp(MDApp):
 
     def build(self):
